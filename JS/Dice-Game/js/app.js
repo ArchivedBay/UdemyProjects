696d36c9e701269c0059ad41af889a95d6c8c5c9
Skyelah Bay <skyelahbay@gmail.com>
2018-05-16 19:20:47 -0500

    Update App.js
    Added functionality for all buttons, discovered bug in newGame() logic
    Created UI function for pushing logic changes onto the DOM - updateScreen()

--- JS/Dice-Game/js/app.js
+++ JS/Dice-Game/js/app.js
@@ -1,45 +1,84 @@
-/*      '#' denotes items completed in this version
+/*   '#' denotes items completed in this version
 # The game has 2 players 
-- In each turn, a player rolls a dice as many times as they wish. 
+# In each turn, a player rolls a dice as many times as they wish. 
     Each result gets added to their ROUND score
-- BUT, if the player rolls a 1, the round score is reset & their turn is over.
-- After that, it's the next player's turn.
-- A player can choose to 'Hold', 
+# BUT, if the player rolls a 1, the round score is reset & their turn is over.
+# After that, it's the next player's turn.
+# A player can choose to 'Hold', 
     which means that their ROUND score gets added to their PLAYER score. 
 - The first player to reach 100 points on PLAYER score wins
+
+TODO: add victory conditions
+FIXME: if new-game is pressed on player 2's turn ,round-score does not reset. look in newGame() logic to find.
 */
 
-
-
-let playerScores, roundScore, activePlayer, diceVal;
 //initial setup
+let playerScores, roundScore, activePlayer, diceVal;
+
 function newGame(){
-    playerScores = [0,0];
-    roundScore = 0;
-    activePlayer = 0;
+    playerScores = [0,0]; //arr[0] = p1      arr[1] = p2
+    roundScore = 0;  //one shared for both players
+    activePlayer = 0; // 0=P1, 1=P2
+    diceVal = 1;
+    updateScreen();
+    // Currently a bug where if game is reset on player 2's turn, roundScore does not update due to improper order here.
 }
-newGame(); // call our newGame() to reset the scores & player.
+function startEventListeners(){
+    let  //attach event listeners to all of our buttons
+        rollButton =    document.querySelector('.btn-roll'),
+        holdButton =    document.querySelector('.btn-hold'),
+        newGameButton = document.querySelector('.btn-new');
+
+    rollButton.addEventListener('click', roll);
+    holdButton.addEventListener('click', addTotalScore);
+    newGameButton.addEventListener('click', newGame);
+}
+newGame(); //immediately call our newGame method to reset all elements.
+startEventListeners(); //start listening for button clicks.
+
+
 
 //Dice logic
 function roll(){
-    let diceEl = document.querySelector('.dice'); //temporarily create a var here.
+     //temporarily create a var here.
     diceVal = Math.floor(Math.random() * 6)+1 //6 sides - 1@ lowest 6@ highest
-    diceEl.src = `images/dice-${diceVal}.png`; //update the DOM
-    return diceVal; //return this data for use later.
+    updateScreen();
+    diceVal === 1 ? resetRound() : addRoundScore(diceVal); //reset round if 1 else add it.
+    return diceVal;
 }
-
-//Score Logic
-function addScore(player, roundScore){
-    //if player pushes hold add roundScore to playerScore
-    playerScores[player] += roundScore;
+//Game logic
+function addTotalScore(){
+    //if player clicks hold add roundScore to playerScore & end turn
+    playerScores[activePlayer] += roundScore;
+    resetRound();
 }
-
+function addRoundScore(points){
+    //if the roll wasn't 1, add the dice value to round score & update
+    roundScore += points;
+    updateScreen();
+}
+function togglePlayer(){ //switch players
+    return activePlayer = activePlayer === 1 ? 0 : 1;
+}
 function resetRound(){
-    roundScore = 0; // if rolled 1 or turn is over.
+    roundScore = 0; // if rolled 1 or turn is over
+    updateScreen();
+    togglePlayer();
 }
 
 
 
+//update the UI itself
+function updateScreen(){
+    let diceEl = document.querySelector('.dice');
+    let rs = document.querySelector(`#current-${activePlayer}`);
+    let p1 = document.querySelector(`#score-${0}`);
+    let p2 = document.querySelector(`#score-${1}`);
+    diceEl.src = `images/dice-${diceVal}.png`;
+    p1.textContent = playerScores[0];
+    p2.textContent = playerScores[1];
+    rs.textContent = roundScore;
+}
 
 
 
